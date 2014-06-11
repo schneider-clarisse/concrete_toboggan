9399e7d1ac91b342035cbd54020e11aa2592db63
Clarisse Schneider <clarissemschneider@gmail.com>
2014-06-10 20:26:31 -0400

    Initial version of new concrete toboggan website, direct copy of canoe website

---
+++ index.php
@@ -0,0 +1,159 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>  
+		<title>WaterlUX</title>
+		<meta name="author" content="Clarisse Schneider">
+        <meta content="text/html; charset=utf-8" http-equiv="content-type">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=false">
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Oleo+Script:400,700' rel='stylesheet' type='text/css'>
+		<link rel="icon" type="image/ico" href="img/favicon.ico"/>
+
+		<link rel="stylesheet" href="style.css">
+		<link href='css/bootstrap-theme.min.css' rel='stylesheet' type='text/css'>
+		<link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+		<link href='css/onepage-scroll.css' rel='stylesheet' type='text/css'>
+		<link href='css/slick.css' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="js/jquery.stellar.js"></script>
+		<script type="text/javascript" src="js/jquery.onepage-scroll.js"></script>
+		<script type="text/javascript" src="js/slick.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		
+	</head>
+	<body>
+		<div class="main"><div class="onepage-wrapper">
+		<section class="page" id="page_one">
+			<nav class="navbar navbar-default" role="banner">
+				<div class="navbar-header">
+			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main_navigation">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			    </div>
+				<div class="navbar-collapse collapse" id="main_navigation">
+					<ul class="nav navbar-nav" role="navigation">
+						<li class="active"><a href="">Home</a></li>
+						<li><a href="/about">Competition</a></li>
+						<li><a href="/competition">Competition</a></li>
+						<li><a href="/sponsorship">Sponsorship</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="logo"><img src="img/logo.png"></div>
+			<p>When pristine lakes meet concrete less dense then water, an engineering tradition is born: a race for victory, powered by student ingenuity.</p>
+			<h3>Welcome to the official website of the University of Waterloo Concrete Canoe Team, located at Canada’s premier institute for engineering.</h3>
+		</section><!-- end page_one -->
+		<section class="page" id="page_two">
+			<div class="slider single-item">
+				<div id="slide_1">
+					<div class="text text-left pull-left">
+						<h3>Canoe Concept</h3>
+						<p>Our journey begins as our hull design leads sketch out rough ideas for the canoe</p>
+					</div>
+				</div>
+				<div id="slide_2">
+					<div class="text text-right pull-right">
+						<h3>Lighter than Water</h3>
+						<p>The special concrete mix used for the canoe must be less dense then water so the canoe can float when totally submerged</p>
+					</div>
+				</div>
+				<div id="slide_3">
+					<div class="text text-left pull-left">
+						<h3>Construction Season</h3>
+						<p>Construction Season – Team members put in long hours to build the canoe formwork</p>
+					</div>
+				</div>
+				<div id="slide_4">
+					<div class="text text-right pull-right">
+						<h3>Pouring 'Crete!</h3>
+						<p>An 8-hour pour is required to cast the concrete and form our canoe!</p>
+					</div>
+				</div>
+			</div>
+		</section><!-- end page_two -->
+		<section class="page" id="page_three">
+			<div class="pull-left" id="twitter-container">
+				<a class="twitter-timeline" href="https://twitter.com/uwtoboggan" data-widget-id="465158525518245889">
+					Tweets by @uwtoboggan
+				</a>
+			</div>
+			<div class="pull-right" id="link-container">
+				<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23F2F2F2&amp;src=cevfjn6i1kgdqu7pncqpvpc62o%40group.calendar.google.com&amp;color=%235F6B02&amp;ctz=America%2FToronto" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+			</div>
+		</section><!-- end page_three -->
+
+		<section class="page" id="page_four">
+			<h1 class="page_title">SPONSORS</h1>
+			<ul class="affiliation_logos">
+				<li><img src="img/supporters/waterloo_engineering.jpg"></li>
+				<li><img src="img/supporters/lakeside.jpg"></li>
+				<li><img src="img/supporters/tacoma.jpg"></li>
+				<li><img src="img/supporters/mte.jpg"></li>
+				<li><img src="img/supporters/waterloo_engsoc.jpg"></li>
+				<li><img src="img/supporters/weef.png"></li>
+				<li><img src="img/supporters/stantec.jpg"></li>
+				<li><img src="img/supporters/golder_associates.jpg"></li>
+				<li><img src="img/supporters/pcl.jpg"></li>
+				<li><img src="img/supporters/dpt_civil.jpg"></li>
+			</ul><!-- end affliation_logos -->
+			<h2><a href="/sponsorship">See all</a></h2>	
+		</section><!-- end page_four -->
+
+		<section class="page" id="page_five">
+			<div id="video-container">
+				<iframe src="//www.youtube.com/embed/wbV2uY237wA" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<footer>
+				<div class="column" id="navigation">
+					<h5>NAVIGATION</h5>
+					<ul class="links">
+						<li><a href="">Home</a></li>
+						<li><a href="/about">Competition</a></li>
+						<li><a href="/competition">Competition</a></li>
+						<li><a href="/sponsorship">Sponsorship</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+				</div>
+				<div class="column" id="links">
+					<h5>LINKS</h5>
+					<ul class="links">
+						<li><a href="http://canoebeton.ca/">CNCCC 2014</a></li>
+					</ul>
+				</div>
+				<div class="column" id="contact">
+					<h5>CONTACT</h5>
+					<ul class="links">
+						<li><a href="mailto:sklinger@uwaterloo.ca">Project Manager</a></li>
+						<li><a href="mailto:sean.reinsma@uwaterloo.ca">Designer</a></li>
+						<li><a href="mailto:jc2murph@uwaterloo.ca">Finance and Sponsorship</a></li>
+						<li><a href="mailto:cmschnei@uwaterloo.ca">Webmaster</a></li>
+					</ul>
+				</div>
+			</footer>
+		</section><!--  end page_five -->
+	</body>
+	<script>
+		$(document).ready(function(){
+			$(window).stellar({
+				 horizontalScrolling: false
+			});
+			$(".main").onepage_scroll({
+		        sectionContainer: "section",
+		        responsiveFallback: 600
+		    });
+		    $('.single-item').slick({
+				  dots: true,
+				  arrows: false,
+				  infinite: true,
+				  speed: 500,
+				  fade: true,
+				  cssEase: 'linear'
+			});
+			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+		});
+	</script>
+</html>
